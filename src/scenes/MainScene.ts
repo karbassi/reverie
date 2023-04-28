5ee001ac2e9715e349a4d733dd9b2bcda113e30b
Ali Karbassi <ali@karbassi.com>
2023-04-28 16:45:52 -0500

    Follow player

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -8,8 +8,6 @@ import {
 } from '@enable3d/phaser-extension'
 import * as THREE from 'three'
 
-const isTouchDevice = 'ontouchstart' in window
-
 export default class MainScene extends Scene3D {
   cams!: {
     ortho: THREE.OrthographicCamera
@@ -19,11 +17,18 @@ export default class MainScene extends Scene3D {
     offset: null
   }
 
-  controls!: ThirdPersonControls
   canJump = true
   canMove = false
   moveTop = 0
   moveRight = 0
+
+  rotation: THREE.Vector3
+  theta: number
+  rotationPlayer: THREE.Vector3
+  thetaPlayer: number
+  speed = 4
+
+  // Controls
   keys!: {
     a: Phaser.Input.Keyboard.Key
     w: Phaser.Input.Keyboard.Key
@@ -31,11 +36,7 @@ export default class MainScene extends Scene3D {
     s: Phaser.Input.Keyboard.Key
     space: Phaser.Input.Keyboard.Key
   }
-  rotation: THREE.Vector3
-  theta: number
-  rotationPlayer: THREE.Vector3
-  thetaPlayer: number
-  speed = 4
+  controls!: ThirdPersonControls
 
   // Scenes
   terraceScene!: THREE.Group
@@ -85,6 +86,7 @@ export default class MainScene extends Scene3D {
     this.addBirds()
 
     // Add controls
+    this.addKeys()
     this.addControls()
 
     // Add collisions
@@ -97,8 +99,6 @@ export default class MainScene extends Scene3D {
 
   private async createWorld() {
     // set up scene (light, ground, grid, sky, orbitControls)
-    this.third.warpSpeed('-light')
-
     const { lights } = await this.third.warpSpeed('-orbitControls', 'ground')
 
     if (lights === undefined) {
@@ -113,22 +113,22 @@ export default class MainScene extends Scene3D {
   }
 
   private createCamera() {
-    const zoom = 10
-    const w = this.cameras.main.width / zoom
-    const h = this.cameras.main.height / zoom
-
-    this.cams = {
-      ortho: this.third.cameras.orthographicCamera({
-        left: w / -2,
-        right: w / 2,
-        top: h / 2,
-        bottom: h / -2,
-      }),
-      perspective: this.third.camera,
-      active: 'perspective',
-      inTransition: false,
-      offset: null,
-    }
+    // this.third.camera.setRotationFromQuaternion(new THREE.Quaternion(10, 10, 0, 0))
+    // const zoom = 10
+    // const w = this.cameras.main.width / zoom
+    // const h = this.cameras.main.height / zoom
+    // this.cams = {
+    //   ortho: this.third.cameras.orthographicCamera({
+    //     left: w / -2,
+    //     right: w / 2,
+    //     top: h / 2,
+    //     bottom: h / -2,
+    //   }),
+    //   perspective: this.third.camera,
+    //   active: 'perspective',
+    //   inTransition: false,
+    //   offset: null,
+    // }
   }
 
   private createScene() {
@@ -174,7 +174,7 @@ export default class MainScene extends Scene3D {
   private createPlayer() {
     // Create player
     this.player = new ExtendedObject3D()
-    this.player.name = 'scene'
+    this.player.name = 'player'
 
     // Add it to the scene
     this.player.add(this.playerScene)
@@ -230,7 +230,7 @@ export default class MainScene extends Scene3D {
   private addBirds() {
     // Create bird
     this.bird = new ExtendedObject3D()
-    this.bird.name = 'scene'
+    this.bird.name = 'bird'
 
     // Add it to the scene
     this.bird.add(this.birdScene)
@@ -299,9 +299,24 @@ export default class MainScene extends Scene3D {
     })
   }
 
+  private addKeys() {
+    if (!this.input.keyboard) {
+      throw new Error('Keyboard not created')
+    }
+
+    // Add Keys
+    this.keys = {
+      a: this.input.keyboard.addKey('a'),
+      w: this.input.keyboard.addKey('w'),
+      d: this.input.keyboard.addKey('d'),
+      s: this.input.keyboard.addKey('s'),
+      space: this.input.keyboard.addKey(32),
+    }
+  }
+
   private addControls() {
     this.controls = new ThirdPersonControls(this.third.camera, this.player, {
-      offset: new THREE.Vector3(0, 1, 0),
+      offset: new THREE.Vector3(0, 0, 0),
       targetRadius: 3,
     })
 
@@ -309,98 +324,37 @@ export default class MainScene extends Scene3D {
     this.controls.theta = 90
 
     // Add Pointer Lock and Pointer Drag
-    if (!isTouchDevice) {
-      let pointerLock = new PointerLock(this.game.canvas)
-      let pointerDrag = new PointerDrag(this.game.canvas)
+    let pointerLock = new PointerLock(this.game.canvas)
+    let pointerDrag = new PointerDrag(this.game.canvas)
 
-      pointerDrag.onMove((delta) => {
-        if (pointerLock.isLocked()) {
-          // FIX?
-          this.player.position.setY(-delta.y)
-          this.moveTop = -delta.y
-          this.moveRight = delta.x
-        }
-      })
-
-      // Add Keys
-
-      this.keys = {
-        a: this.input.keyboard.addKey('a'),
-        w: this.input.keyboard.addKey('w'),
-        d: this.input.keyboard.addKey('d'),
-        s: this.input.keyboard.addKey('s'),
-        space: this.input.keyboard.addKey(32),
+    pointerDrag.onMove((delta) => {
+      if (!pointerLock.isLocked()) {
+        return
       }
 
-      //Adding joystick
-      if (isTouchDevice) {
-        const joystick = new JoyStick()
-
-        const axis = joystick.add.axis({
-          styles: { left: 35, bottom: 35, size: 100 },
-        })
-
-        axis.onMove((event) => {
-          // Update Camera
-          this.moveTop = event.x * 3
-          this.moveRight = event.y * 3
-        })
-
-        const buttonA = joystick.add.button({
-          letter: 'A',
-          styles: { right: 35, bottom: 110, size: 80 },
-        })
-
-        buttonA.onClick(() => this.jump())
-
-        const buttonB = joystick.add.button({
-          letter: 'B',
-          styles: { right: 110, bottom: 35, size: 80 },
-        })
-
-        buttonB.onClick(() => (this.canMove = true))
-        buttonB.onRelease(() => (this.canMove = false))
-      }
-    }
-  }
-
-  jump() {
-    if (!this.player || !this.canJump) {
-      return
-    }
-
-    this.canJump = false
-    this.player.anims.play('jump_running', 500, false)
-
-    this.time.addEvent({
-      delay: 650,
-      callback: () => {
-        this.canJump = true
-        this.player.anims.play('idle')
-      },
+      this.moveTop = -delta.y
+      this.moveRight = delta.x
     })
-
-    this.player.body.applyForceY(6)
   }
 
   update(time, delta) {
-    if (this.player && this.player.body) {
-      // Update Controls
-
-      const deltaX = this.moveRight * 2
-      const deltaY = this.moveTop * 2
-
-      // this.controls.update(deltaX, deltaY)
-
-      if (!isTouchDevice) {
-        this.moveRight = 0
-        this.moveTop = 0
-      }
-
-      // this.playerTurn()
-      // this.playerMove()
-      // this.playerJump()
+    // Early return if no player or player body
+    if (!this.player || !this.player.body) {
+      return
     }
+
+    // Update Controls
+    const deltaX = this.moveRight * 2
+    const deltaY = -this.moveTop * 2
+    this.controls.update(deltaX, deltaY)
+
+    // Update Player
+    this.moveRight = 0
+    this.moveTop = 0
+
+    this.playerTurn()
+    this.playerMove()
+    this.playerJump()
   }
 
   playerTurn() {
@@ -415,7 +369,7 @@ export default class MainScene extends Scene3D {
     this.player.body.setAngularVelocityY(0)
 
     const l = Math.abs(this.theta - this.thetaPlayer)
-    let rotationSpeed = isTouchDevice ? 2 : 4
+    let rotationSpeed = 4
     let d = Math.PI / 24
 
     if (l > d) {
@@ -432,11 +386,7 @@ export default class MainScene extends Scene3D {
   }
 
   playerMove() {
-    // if (this.canMove === false) {
-    //   return
-    // }
-
-    //Player Move
+    // Player Move
 
     if (this.keys.w.isDown) {
       this.playerMoveForward()
@@ -460,10 +410,22 @@ export default class MainScene extends Scene3D {
   }
 
   playerJump() {
-    //Player Jump
-
-    if (this.keys.space.isDown && this.canJump) {
-      this.jump()
+    if (!this.keys.space.isDown || !this.canJump) {
+      return
     }
+
+    // Player Jump
+    this.canJump = false
+    this.player.anims.play('jump_running', 500, false)
+
+    this.player.body.applyForceY(6)
+
+    this.time.addEvent({
+      delay: 650,
+      callback: () => {
+        this.canJump = true
+        this.player.anims.play('idle')
+      },
+    })
   }
 }
