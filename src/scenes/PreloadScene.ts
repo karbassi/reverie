43591cdae86c19f8eb0b44bd78c78d953aa8e013
Ali Karbassi <ali@karbassi.com>
2023-04-28 16:09:03 -0500

    Add GLB file loading to preloader

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -20,14 +20,6 @@ export default class PreloadScene extends Scene3D {
     await this.third.load.preload('terrace', '/assets/glb/terrace.glb')
     await this.third.load.preload('player', '/assets/glb/idle.glb')
     await this.third.load.preload('bird', '/assets/glb/bird.glb')
-
-    // this.terrace = await this.third.load.gltf('/assets/glb/terrace.glb')
-    // this.player = await this.third.load.gltf('/assets/glb/idle.glb')
-    // this.bird = await this.third.load.gltf('/assets/glb/bird.glb')
-    // this.birdfly = this.third.load.gltf('/assets/glb/birdfly.glb')
-
-    // console.log(this.player.object.scenes[0])
-    // console.log(this.bird.object.scenes[0])
   }
 
   async create() {
